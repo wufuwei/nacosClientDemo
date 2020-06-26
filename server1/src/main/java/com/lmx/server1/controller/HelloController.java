6a85b1bf8820d6ddc6ef28149c3d3b9c8b01f38c
lmx <liumingxin@mininglamp.com>
2020-06-26 09:54:06 +0800

    nacos 配置文件 热更新 完成

--- server1/src/main/java/com/lmx/server1/controller/HelloController.java
+++ server1/src/main/java/com/lmx/server1/controller/HelloController.java
@@ -1,6 +1,10 @@
 package com.lmx.server1.controller;
 
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,14 +13,23 @@ import org.springframework.web.bind.annotation.RestController;
  * @create: 2020/6/25
  **/
 @RestController
+@RefreshScope
 public class HelloController {
 
     @Value("${server.port}")
     private Integer port;
 
+    @Value("${demo.name}")
+    private String name;
+
     @GetMapping("/hello")
     public String getHello(){
         return "hello this is port" + port;
+    }
+
+    @GetMapping("/name")
+    public String userName(){
+        return "this nacos name :" + name;
     }
 
 }
